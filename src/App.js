1e553e75d18b97567bba67a25f6d6bfc91f6ce74
TrungMai <anh.maitrung@gmail.com>
2023-02-25 14:08:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Router, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import { createBrowserHistory } from 'history';
+import '../src/css/style.css'
+
+export const history = createBrowserHistory()
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path={'/home'} component={Home}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/redux/configStore.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { reducer } from "./reducers/reducer";
+
+
+
+
+const rootReducer = combineReducers({
+    reducer,
+})
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
