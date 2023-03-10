fbb7c243004a59d0669f7d2ced9da94632d20983
TrungMai <anh.maitrung@gmail.com>
2023-03-11 00:25:29 +0700

    use redux

--- src/redux/reducers/ProjectManagementReducer.js
+++ src/redux/reducers/ProjectManagementReducer.js
@@ -1,11 +1,15 @@
-const initialState = {}
+import { GET_PROJECT } from "../../ulti/setting"
+
+const initialState = []
 
 export const ProjectManagementReducer = (state = initialState, action) => {
-  switch (action.type) {
+    switch (action.type) {
+        case GET_PROJECT:
+            // console.log(action.data)
+            return [...action.data]
 
-
-
-  default:
-    return state
-  }
+        default:
+            return state
+    }
 }
+
