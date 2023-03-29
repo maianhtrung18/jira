94880faeed0abe7ad410e54037901f89b1de80ce
TrungMai <anh.maitrung@gmail.com>
2023-03-29 22:05:09 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import { Route, Router, Switch } from 'react-router-dom';
+import { Router, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import '../src/css/style.css'
 import FormTemplate from './templates/FormTemplate';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import Header from './pages/components/Header';
 import ProjectManagerTemplate from './templates/ProjectManagerTemplate';
 import ProjectManager from './pages/projectManager/ProjectManager';
 import User from './pages/users/User';
@@ -21,7 +20,6 @@ function App() {
       <Switch>
         <FormTemplate exact path='/login' component={Login}/>
         <FormTemplate exact path='/register' component={Register}/>
-        {/* <Route exact path={'/home'} component={Header}/> */}
         <ProjectManagerTemplate exact path='/home' component={ProjectManager}/>
         <ProjectManagerTemplate exact path='/user' component={User}/>
         <ProjectManagerTemplate exact path='/' component={ProjectManager}/>
